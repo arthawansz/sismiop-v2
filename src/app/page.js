b15e995b25abe8c782525b693fdfd7269f82dc65
cuyngoding <arthwanpratama1@gmail.com>
2025-12-23 23:54:07 +0800

    progres login page 50%

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,108 @@
+import React from "react";
 import Image from "next/image";
+import BgLogin from "./assets/bglogin.png";
+import LogoKabPinrang from "./assets/LogoKabPinrang.png";
 
-export default function Home() {
+function page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <div className="login-page relative flex min-h-screen text-white">
+        {/* Background */}
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          src={BgLogin}
+          alt="background"
+          fill
+          className="object-cover"
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
+
+        {/* BLUR LAYER */}
+        <div
+          className="
+      absolute inset-y-0 left-0 w-[65%]
+      backdrop-blur-[3px]
+      bg-linear-to-r from-black/90 via-black/50 to-black/0
+      [mask-image:linear-gradient(to_right,black_60%,black_40%,transparent_100%)]
+      [-webkit-mask-image:linear-gradient(to_right,black_80%,black_50%,transparent_100%)]
+      z-10
+    "
+        />
+
+        {/* left side */}
+        <div className="relative left-10 z-20 w-[80%] p-10">
+          <div className="flex items-center gap-4">
+            <div className="bg-white p-3 rounded-lg">
+              <Image
+                src={LogoKabPinrang}
+                alt="logo kabupaten pinrang"
+                width={40}
+                height={40}
+                className="object-contain"
+              />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-[16px] font-semibold">
+                Pemerintah Kabupaten PINRANG
+              </p>
+
+              <p className="text-sm font-medium text-gray-300">
+                BAPENDA Kabupaten PINRANG
+              </p>
+            </div>
+          </div>
+          <h1 className="mt-6 w-125 text-[42px] font-bold">
+            Pengelolaan PBB yang Terstruktur, Transparan, dan Efisien
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+
+          <ul className="mt-9 flex flex-col">
+            {/* Menggunakan grid agar kolom angka dan label sejajar sempurna */}
+
+            {/* Item 1 */}
+            <li className="grid grid-cols-[360px_1fr] border-b border-white/20 w-125 items-center py-4">
+              <p className="text-[32px] font-medium">123.213</p>
+              <p className="text-[14px] text-gray-300 mt-5">Jumlah Objek Pajak</p>
+            </li>
+
+            {/* Item 2 */}
+            <li className="grid grid-cols-[360px_1fr] border-b border-white/20 w-125 items-center py-4">
+              <p className="text-[32px] font-medium">159.204</p>
+              <p className="text-[14px] text-gray-300 mt-5">Jumlah Wajib Pajak</p>
+            </li>
+
+            {/* Item 3 */}
+            <li className="grid grid-cols-[360px_1fr] w-125 items-center py-4">
+              <p className="text-[32px] font-medium">0</p>
+              <p className="text-[14px] text-gray-300 mt-5">Jumlah SPPT Aktual</p>
+            </li>
+          </ul>
+
+          <p className="text-sm text-gray-300 mt-10 w-1/2 font-normal">Jl. Pemuda, Laloeha, Kolaka, Kabupaten Kolaka Utara, Sulawesi Tenggara 93561, Indonesia</p>
+          <br/>
+
+          <div className="flex flex-row gap-3">
+
+          <p className="text-sm text-gray-300">Copyright © Nusantara Media Informatika</p>
+          {/* berikan titik tengah sebagai pembatas element dengan warna white/20 */}
+          <div className="bg-white/20 w-[6.5px] h-[6.5px] rounded-full mt-2"></div>
+          <a href="#" className="text-white text-sm font-semibold hover:underline">Syarat & ketentuan</a>
+
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        {/* Right Side */}
+        <div className="md:w-1/3 w-full flex justify-center relative bottom-10">
+        <div className="bg-white rounded-[20px] shadow-2xl shadow-black w-120 h-150 z-10 relative top-32 right-40">
+        
+          <form>
+
+          </form>
         </div>
-      </main>
-    </div>
+        
+        
+        </div>
+      </div>
+    </>
   );
 }
+
+export default page;
